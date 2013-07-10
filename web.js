5cf4fe376cd98476e8147d6158ab73a05d1c7369
thelvaci <helvacitahir@gmail.com>
2013-07-10 13:59:19 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require('fs');
+var infile = "index.html";
+var Content= String(fs.readFileSync(infile,'utf8'));
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(Content);
 });
 
 var port = process.env.PORT || 5000;
